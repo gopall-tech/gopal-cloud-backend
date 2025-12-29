3dd80e41f1f446afcb20f4dca1db7cb6e948cf4d
gopall-tech <gopal.walia@techconsulting.tech>
2025-12-29 09:24:55 -0500

    Add Network Module

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = [var.address_space]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  # CHANGED: Added Gopal- prefix
+  name                 = "Gopal-snet-aks"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.aks_subnet_prefix]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  # CHANGED: Added Gopal- prefix
+  name                 = "Gopal-snet-db"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.db_subnet_prefix]
+
+  delegation {
+    name = "fs"
+    service_delegation {
+      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,6 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "vnet_name" { type = string }
+variable "address_space" { type = string }
+variable "aks_subnet_prefix" { type = string }
+variable "db_subnet_prefix" { type = string }
