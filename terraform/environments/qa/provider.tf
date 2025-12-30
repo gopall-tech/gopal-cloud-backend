b75b5484b1e3e0c24367725742dc36d188bd22be
gopall-tech <gopal.walia@techconsulting.tech>
2025-12-30 11:20:38 -0500

    Configure remote backend

--- terraform/environments/qa/provider.tf
+++ terraform/environments/qa/provider.tf
@@ -9,8 +9,19 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  # --- ADD THIS BLOCK ---
+  backend "azurerm" {
+    resource_group_name  = "Gopal-tfstate-rg"
+    storage_account_name = "gopalstate87215"
+    container_name       = "tfstate"
+    key                  = "qa.terraform.tfstate"
+    use_oidc             = true
+  }
+  # ----------------------
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
